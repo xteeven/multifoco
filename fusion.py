fd781d20bb666445059e7b28410ca4dadb0841ea
xteeven <david7246@gmail.com>
2015-09-29 14:51:00 -0500

    resultados

--- fusion.py
+++ fusion.py
@@ -14,7 +14,7 @@ xn = x / norm
 vector = mlpy.LibSvm(svm_type='nu_svc', kernel_type='rbf', gamma=900 , C=1)
 vector.learn(xn, y)
 
-numero =29
+numero =10
 files = GetImagenes("dataset_gray")
 img = numero*2
 A = v.imread('dataset_gray/'+files[img], 0)
