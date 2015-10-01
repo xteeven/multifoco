c37a19671c0ffd106e66844c5074ad42380d8e49
xteeven <david7246@gmail.com>
2015-10-01 14:24:57 -0500

    optimizacion de descriptores se agrego gradiente morfologico

--- Caracteristicas.py
+++ Caracteristicas.py
@@ -5,57 +5,59 @@ import cv2 as v
 import numpy as np
 import time
 
-imagen = v.imread('1.jpg',0)
+imagen = v.imread('OUT/3_1_00_I.jpg',0)
 
-#convolucion con kernel de laplace energy  segun Wei Huang, Zhongliang Jing
 
-#v.imshow('2',energy)
 
-def eolaplace(image):
+def eolaplace(image, value=True):
     energy = v.filter2D(image, -1, np.asarray(([-1,-4,-1],[-4,20,-4],[-1,-4,-1])))
-    return sum(sum(energy**2))*1.0/((len(image))*(len(image[0])))*1.0
-
-def eol(imagen):
-    sumenergy = 0
-    imagen = v.copyMakeBorder(imagen, 1, 1, 1, 1, v.BORDER_CONSTANT)
-    for x in range(1, len(imagen[0])-1):
-        for y in range(1, len(imagen)-1):
-            fxxfyy = 20L*imagen[y][x]-imagen[y-1][x-1]-imagen[y+1][x+1]-imagen[y+1][x-1]-imagen[y-1][x+1]-4*imagen[y-1][x]-4*imagen[y+1][x]-4*imagen[y][x-1]-4*imagen[y][x+1]
-            sumenergy += fxxfyy**2
-    return sumenergy/((len(imagen)-1)*(len(imagen[0])-1))
-
-#definicion 2: partiendo del histograma segun Carlos Platero.
-def eol2(imagen):
-    sumenergy = 0
-    laplace = v.Laplacian(imagen, v.CV_16S)
-    laplace = v.convertScaleAbs(laplace)
-    histograma = v.calcHist(laplace, [0], None, [256], [0, 256])
-    p = histograma/(len(imagen[0])*len(imagen))
-    for i in range(len(p)):
-        sumenergy += p[i]**2
-    return sumenergy
-
-#Suma laplaciano modificado segun Wei Huang, Zhongliang Jing
-def smlaplacian(imagen, border=1):
-    sumlaplacian = 0
-    imagen = v.copyMakeBorder(imagen, border, border, border, border, v.BORDER_CONSTANT)
-    for x in range(border, len(imagen[0])-border):
-        for y in range(border, len(imagen)-border):
-            modified = abs(2*imagen[y][x]-imagen[y][x-border]-imagen[y][x+border])+abs(2*imagen[y][x]-imagen[y-border][x]-imagen[y+border][x])*1L
-            sumlaplacian += modified
-    return sumlaplacian/((len(imagen)-1)*(len(imagen[0])-1))*1.0
+    energy = np.array(np.array(energy, dtype=np.uint16)**2)
+    energy = (energy/float(np.amax(energy)))*float(255)
+    if value:
+        return sum(sum(energy))/((len(image))*(len(image[0])))*1.0
+    else:
+        return np.array(energy, dtype=np.uint8)
 
 
-
-def eogradient(imagen):
-    sumgradient = 0
-    imagen = v.copyMakeBorder(imagen, 1, 1, 1, 1, v.BORDER_CONSTANT)
-    for x in range(1, len(imagen[0])-1):
-        for y in range(1, len(imagen)-1):
-            fx = imagen[y][x+1]*1L-imagen[y][x]*1L
-            fy = imagen[y+1][x]*1L-imagen[y][x]*1L
-            sumgradient += fx**2+fy**2
-    return sumgradient/((len(imagen)-1)*(len(imagen[0])-1))*1.0
+def smlaplacian(imagen, stepx=1, stepy=1,value=True):
+    kernx = np.array([[-1]+[0]*stepy+[2]+[0]*stepy+[-1]]).astype(np.float)
+    kerny = np.array([[-1]+[0]*stepx+[2]+[0]*stepx+[-1]]).astype(np.float).T
+    energy = v.filter2D(imagen,-1, kerny) + v.filter2D(imagen,-1, kernx)
+    if value:
+        return float(sum(sum(energy)))/((len(imagen))*(len(imagen[0])))
+    else:
+        return energy
 
 
+def eogradient(imagen, value=True):
+    eog = (np.array(v.Sobel(imagen, -1, 0, 1), dtype=np.uint16)**2 + np.array(v.Sobel(imagen, -1, 1, 0), dtype=np.uint16)**2)
+    eog = np.array((eog/float(np.amax(eog)))*float(255))
+    if value:
+        return sum(sum(np.array(eog)))/((len(imagen))*(len(imagen[0])))*1.0
+    else:
+        return np.array(eog, dtype=np.uint8)
+
+def morphgradient(imagen, size = 3, value=True):
+    it=1
+    mgd = v.dilate(imagen, v.getStructuringElement(v.MORPH_RECT, (size, size)),iterations=it)-v.erode(imagen, v.getStructuringElement(v.MORPH_RECT, (size, size)), iterations=it)
+    rt, bin = v.threshold(mgd, 128, 255, v.THRESH_BINARY)
+    if value:
+        return float(sum(sum(mgd)))/((len(imagen))*(len(imagen[0])))*1.0
+    else:
+        return mgd
+
+def varianza(imagen):
+    return float(np.var(imagen))/((len(imagen))*(len(imagen[0])))*1.0
+
+
+print smlaplacian(imagen)
+print eolaplace(imagen)
+print eogradient(imagen)
+print morphgradient(imagen)
+print varianza(imagen)
+v.imshow('1',smlaplacian(imagen, value=False))
+v.imshow('2',eolaplace(imagen, value=False))
+v.imshow('3', eogradient(imagen, value=False))
+v.imshow('4', morphgradient(imagen, value=False))
+v.imshow('0',imagen)
 v.waitKey()
